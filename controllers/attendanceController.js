b58a809c1fde2f9f37d3e9ae7e5d9bf381cac430
parvati awrul <parvati23@navgurukul.org>
2025-10-25 20:13:49 +0530

    added three api

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -44,3 +44,46 @@ export const markKitchenTurn = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+
+export const getTodayAttendance = async (req, res) => {
+  try {
+    const studentId = req.user.id;
+    const today = new Date().toISOString().split("T")[0];
+
+    const record = await Attendance.findOne({ student: studentId, date: today });
+
+    if (!record) {
+      return res.status(200).json({
+        message: "No attendance marked yet for today.",
+        slots: {
+          slot1: "Not Marked",
+          slot2: "Not Marked",
+          slot3: "Not Marked",
+        },
+      });
+    }
+
+    res.status(200).json({
+      message: "Today's attendance fetched successfully.",
+      record,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+export const getAttendanceHistory = async (req, res) => {
+  try {
+    const { studentId } = req.params;
+    const records = await Attendance.find({ student: studentId })
+      .sort({ date: -1 }); 
+
+    if (!records || records.length === 0) {
+      return res.status(404).json({ message: "No attendance records found" });
+    }
+
+    res.status(200).json(records);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
-import { markQRAttendance, markKitchenTurn } from "../controllers/attendanceController.js";
+import { markQRAttendance, markKitchenTurn ,getTodayAttendance,getAttendanceHistory } from "../controllers/attendanceController.js";
+
 
 const router = express.Router();
 
@@ -8,6 +9,10 @@ const router = express.Router();
 router.post("/mark", verifyToken, markQRAttendance);
 
 router.post("/kitchen", verifyToken, markKitchenTurn);
+router.get("/today", verifyToken, getTodayAttendance);
+router.get("/history/:studentId", verifyToken, getAttendanceHistory);
+
+
 
 export default router;
 
